7a8693614fe9ddd23739dd3cb51dc3952f1f971e
Yasin Özcan <yasin.ozcan@demirorenteknoloji.com>
2019-10-24 17:20:17 +0300

    added base page component

---
+++ src/app/pages/base/page.ts
@@ -0,0 +1,16 @@
+
+export interface IOnLoadable {
+	onLoad(): void;
+	onFullLoad(): void;
+}
+
+export abstract class Page implements IOnLoadable {
+
+	constructor() {
+		console.log('ssss')
+	}
+	
+	public onLoad() : void {}
+	public onFullLoad() : void {}
+
+}
--- src/app/pages/news-detail/index.ts
+++ src/app/pages/news-detail/index.ts
@@ -1,6 +1,28 @@
-const test:string = "asdadsas";
-alert(test);
+import { Page } from "../base/page";
 import { Button_1 } from "../../components/buttons/buttons";
+import "./styles/test.scss";
 
-const button = new Button_1("deneme5");
-alert('ssss');
+export class NewsDetailPage extends Page {
+
+    private StringUpperCase(text:string){
+         return text.toUpperCase();
+    }
+
+    constructor () {
+        super();
+        this.onLoad();
+        this.onFullLoad();
+    }
+
+    onLoad(): void {
+        const test:string = "asdadsas";
+        const button = new Button_1("deneme5");
+        console.log(this.StringUpperCase("testdeneme"));
+    }
+
+    onFullLoad(): void {
+        console.log('test');
+	}   
+}
+
+new NewsDetailPage;
